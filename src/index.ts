d7f5413f23b7ec1e1ee88c96b4295af48623a07e
Weeco <weeco91@gmail.com>
2018-05-05 12:13:53 +0200

    Export clan wars interface

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './cr-api';
 export * from './interfaces/cards';
 export * from './interfaces/clan-leaderboard';
 export * from './interfaces/clan-profile';
+export * from './interfaces/clan-wars';
 export * from './interfaces/common';
 export * from './interfaces/locations';
 export * from './interfaces/player-battle-logs';
